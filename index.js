ed089b1eb787d2e83b015998228b822c925a97e6
Sophie DeBenedetto <sophie.debenedetto@gmail.com>
2018-08-08 22:19:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,26 +5,7 @@ function handleFormSubmit(event) {
  alert("thanks for submitting the form")
 }
 
-function fetchCurrentWeather(city) {
- 
-}
 
-function displayCurrentWeather(json) {
-document.getElementById("temp").value(json.main.temp)
-}
-
-
-function fetchFiveDayForecast(city) {
-  //fetch five day forecast data based on city
-}
-
-function displayFiveDayForecast(json) {
-  //render five day forecast data to the DOM using provided IDs and json from API
-}
-
-function createChart(json) {
-  //Bonus: render temperature chart using five day forecast data and ChartJS
-}
 
 document.addEventListener('DOMContentLoaded', function() {
  // add the event listener
