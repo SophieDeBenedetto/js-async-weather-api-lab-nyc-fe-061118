ecb7a5d6eaa76a970c6ce5f5e0889946db623bce
Sophie DeBenedetto <sophie.debenedetto@gmail.com>
2018-08-08 22:28:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function handleFormSubmit(event) {
 
 function getWeatherFromApi(city) {
   debugger;
-  fetch("api.openweathermap.org/data/2.5/weather?q=" + city)
+  fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city)
     .then(response => response.json())
     .then(displayWeather)
 }
