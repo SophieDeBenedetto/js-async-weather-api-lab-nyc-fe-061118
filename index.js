fec51218b9149773836f80ebc0846161d44450ec
Sophie DeBenedetto <sophie.debenedetto@gmail.com>
2018-08-08 22:22:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const API_KEY  = "5357d0ee7dd6ec614e764228b9249e29"
 const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?`
 
 function handleFormSubmit(event) {
- alert("thanks for submitting the form")
+ debugger;
+ // next I need to send a web request to this URL: api.openweathermap.org/data/2.5/weather?q=London
+ // I need to substitute "London" for whatever city the user filled out. 
 }
 
 
