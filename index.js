a16ef4dc456ff5b7b317071c32d7c1d85e5f92cc
Sophie DeBenedetto <sophie.debenedetto@gmail.com>
2018-08-08 22:11:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,19 +2,11 @@ const API_KEY  = "5357d0ee7dd6ec614e764228b9249e29"
 const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?`
 
 function handleFormSubmit(event) {
-  event.preventDefault();
-  let city = document.getElementById("city").value
-  fetchCurrentWeather(city)
+ 
 }
 
 function fetchCurrentWeather(city) {
- // use fetch to make a request to the weather api to get weather for this city
- let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&type=like&units=imperial&APPID=${API_KEY}`
- fetch(url)
-  .then(function(response) {
-    return response.json()
-  })
-  .then(displayCurrentWeather)
+ 
 }
 
 function displayCurrentWeather(json) {
@@ -36,8 +28,7 @@ function createChart(json) {
 
 document.addEventListener('DOMContentLoaded', function() {
  // add the event listener
- document.getElementById("cityForm").addEventListener("submit", handleFormSubmit)
-  // $("form").on("submit", handleFormSubmit)
+ 
 })
 
 
